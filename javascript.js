df2a5e45b11ae0c7c5fd9202a92e6f92d30f4e54
PaarthAsri <116148446+PaarthAsri@users.noreply.github.com>
2023-11-09 23:48:11 +0530

    all files added

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px"; // Adjust the width as needed
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
